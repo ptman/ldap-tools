65c781aea9a3f60ba1025f7c174f56ff804b219e
Paul Tötterman <paul.totterman@iki.fi>
2011-03-09 00:01:39 +0200

    Add support for views

--- ldap2zone.py
+++ ldap2zone.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 #
-# Copyright (c) 2009, Paul Tötterman <paul.totterman@iki.fi>
+# Copyright (c) 2011, Paul Tötterman <paul.totterman@iki.fi>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -70,7 +70,8 @@ SOA_ATTRIBUTES = {'sOANameServer': 'ns',
                   'sOARefresh':    'refresh',
                   'sOARetry':      'retry',
                   'sOAExpire':     'expire',
-                  'sOANegCache':   'negcache'}
+                  'sOANegCache':   'negcache',
+                  'defaultTTL':    'dttl'} # doesn't actually belong here
 SOA_DEFAULTS = {'refresh':  1200, # RFC1912
                 'retry':    180,
                 'expire':   1209600, # RFC1912
@@ -109,21 +110,18 @@ def attribute2type(attribute):
     '''Convert LDAP attribute name to DNS record type.'''
     return attribute.split('Record')[0].upper()
 
-def output_soa(entries, zone_entry):
+def output_soa(zone_entry, computed_serial):
     '''Output SOA record based on existing values in zone entry or guesses.'''
-    if 'sOARecord' in zone_entry[1]:
-        output_record({'name': zone_entry[1]['relativeDomainName'][0],
+    if 'sOARecord' in zone_entry:
+        output_record({'name': zone_entry['relativeDomainName'][1][0],
                        'type': attribute2type('sOARecord'),
-                       'data': zone_entry[1]['sOARecord'][0]})
+                       'data': zone_entry['sOARecord'][1][0]})
     else:
         soa_values = SOA_DEFAULTS.copy()
+        soa_values['serial'] = computed_serial
         for attr in SOA_ATTRIBUTES.keys():
-            if attr in zone_entry[1]:
-                soa_values[SOA_ATTRIBUTES[attr]] = zone_entry[1][attr][0]
-
-        if not 'serial' in soa_values:
-            epochs = [mtime2epoch(e[1]['modifyTimestamp'][0]) for e in entries]
-            soa_values['serial'] = max(epochs)
+            if attr in zone_entry:
+                soa_values[SOA_ATTRIBUTES[attr]] = zone_entry[attr][1][0]
 
         output_record({'name': '@',
                        'ttl':  '',
@@ -132,39 +130,71 @@ def output_soa(entries, zone_entry):
                                 '%(retry)s %(expire)s %(negcache)s)' %
                                 soa_values)})
 
-def generate_zonefile(ds, base, zone):
+def generate_zonefile(ds, base, zone, views):
     '''Generates the zonefile from ldap. Does the actual work.'''
-    # pylint: disable-msg=C0103
-    filterstr = '(&(objectClass=dNSRecord)(zoneName=%s))' % zone
-    attrs = ['relativeDomainName', 'modifyTimestamp', 'sOARecord',
-             'sOANameServer', 'sOAEmail', 'sOASerial', 'sOARetry',
-             'sOAExpire', 'sOANegCache', 'defaultTTL', 'dNSView',
-             'dNSTTL'] + RECORD_ATTRIBUTES
-    entries = ds.search_s(base, ldap.SCOPE_SUBTREE, filterstr, attrs)
+    # pylint: disable-msg=C0103,R0912,R0914
+    unified_entries = {}
 
-    zone_entry = [e for e in entries if e[1]['relativeDomainName'][0] == '@'][0]
+    computed_serial = 0
+
+    if views == []:
+        views = ['']
+
+    result_attrs = ['relativeDomainName', 'modifyTimestamp', 'dNSView',
+                    'dNSTTL',
+                 'defaultTTL'] + RECORD_ATTRIBUTES + SOA_ATTRIBUTES.keys()
+    for view in views:
+        if view != '':
+            filterstr = ('(&(objectClass=dNSRecord)(zoneName=%(zone)s)'
+                         '(dNSView=%(view)s))' % {'zone': zone, 'view': view})
+        else:
+            filterstr = '(&(objectClass=dNSRecord)(zoneName=%s))' % zone
+        entries = ds.search_s(base, ldap.SCOPE_SUBTREE, filterstr, result_attrs)
+
+        epochs = [mtime2epoch(e[1]['modifyTimestamp'][0]) for e in entries]
+        epochs.append(computed_serial)
+        computed_serial = max(epochs)
+
+        for entry in entries:
+            attrs = entry[1]
+            for name in attrs['relativeDomainName']:
+                if name in unified_entries and unified_entries[name][0] != view:
+                    del unified_entries[name]
+                if not name in unified_entries:
+                    unified_entries[name] = (view, {})
+                unified_attrs = unified_entries[name][1]
+                for attr in RECORD_ATTRIBUTES + SOA_ATTRIBUTES.keys():
+                    ttl = None
+                    if 'dNSTTL' in attrs:
+                        ttl = attrs['dNSTTL'][0]
+                    if attr in attrs:
+                        unified_attrs[attr] = (ttl, attrs[attr])
+
+    zone_entry = unified_entries['@'][1]
 
     default_ttl = 60
-    if 'defaultTTL' in zone_entry[1]:
-        default_ttl = zone_entry[1]['defaultTTL'][0]
+    if 'defaultTTL' in zone_entry:
+        default_ttl = zone_entry['defaultTTL'][1][0]
 
     output_header(zone, default_ttl)
 
-    output_soa(entries, zone_entry)
+    output_soa(zone_entry, computed_serial)
 
-    for entry in entries:
-        attrs = entry[1]
+    for name in unified_entries:
+        attrs = unified_entries[name][1]
         ttl = ''
+
         if 'dNSTTL' in attrs and attrs['dNSTTL'][0] != default_ttl:
             ttl = attrs['dNSTTL'][0]
-        for name in attrs['relativeDomainName']:
-            for attr in RECORD_ATTRIBUTES:
-                if attr in attrs:
-                    for value in attrs[attr]:
-                        output_record({'name': name,
-                                       'ttl':  ttl,
-                                       'type': attribute2type(attr),
-                                       'data': value})
+        for attr in RECORD_ATTRIBUTES:
+            if attr in attrs:
+                if attrs[attr][0] is not None and attrs[attr][0] != default_ttl:
+                    ttl = attrs[attr][0]
+                for value in attrs[attr][1]:
+                    output_record({'name': name,
+                                   'ttl':  ttl,
+                                   'type': attribute2type(attr),
+                                   'data': value})
 
 def run():
     '''The main function'''
@@ -194,6 +224,12 @@ def run():
                       action='store',
                       dest='zone',
                       help='Name of the zone')
+    parser.add_option('-v',
+                      '--view',
+                      action='append',
+                      dest='views',
+                      default=[],
+                      help='View(s) to include')
 
     try:
         import optcomplete
@@ -224,7 +260,7 @@ def run():
     if opts.binddn:
         ds.simple_bind_s(opts.binddn, opts.bindpw)
 
-    generate_zonefile(ds, base, zone)
+    generate_zonefile(ds, base, zone, opts.views)
 
 if __name__ == '__main__':
     run()
